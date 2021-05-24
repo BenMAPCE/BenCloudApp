8d807b2a2be61c1ab251501ead123e5f264a092c
Jim Anderton <janderton@indecon.com>
2021-05-24 15:32:11 -0700

    Initial implementation of air quality API methods

--- src/main/java/gov/epa/bencloud/api/util/AirQualityUtil.java
+++ src/main/java/gov/epa/bencloud/api/util/AirQualityUtil.java
@@ -2,4 +2,28 @@ package gov.epa.bencloud.api.util;
 
 public class AirQualityUtil {
 
+	public static String validateModelColumnHeadings(int columnIdx, int rowIdx, int metricIdx, int seasonalMetricIdx, int statisticIdx, int valuesIdx) {
+		StringBuilder b = new StringBuilder();
+		if(columnIdx == -999) {
+			b.append((b.length()==0 ? "" : ", ") + "Column");
+		}
+		if(rowIdx == -999) {
+			b.append((b.length()==0 ? "" : ", ") + "Row");
+		}
+		if(metricIdx == -999) {
+			b.append((b.length()==0 ? "" : ", ") + "Metric");
+		}
+		if(seasonalMetricIdx == -999) {
+			b.append((b.length()==0 ? "" : ", ") + "Seasonal Metric");
+		}
+		if(statisticIdx == -999) {
+			b.append((b.length()==0 ? "" : ", ") + "Statistic");
+		}
+		if(valuesIdx == -999) {
+			b.append((b.length()==0 ? "" : ", ") + "Values");
+		}
+		
+		return b.toString();
+	}
+
 }
