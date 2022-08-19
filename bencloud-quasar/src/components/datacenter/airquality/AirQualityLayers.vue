8b60b3c178b2ca5e30246f9805d4f40dd85c8e27
ADriver <adrian.driver@noaa.gov>
2022-08-19 13:57:59 -0700

    Update to AQ layer delete icon, hide when share_scope = 1

--- bencloud-quasar/src/components/datacenter/airquality/AirQualityLayers.vue
+++ bencloud-quasar/src/components/datacenter/airquality/AirQualityLayers.vue
@@ -16,7 +16,7 @@
     <template v-slot:body="props">
       <q-tr class="cursor-pointer" :props="props" @click.exact="rowClicked(props)">
         <q-td v-for="col in props.cols" :key="col.name" :props="props">
-          <template v-if="col.name === 'actions'">
+          <template v-if="col.name === 'actions' & props.row.share_scope != 1">
             <q-btn
               dense
               round
