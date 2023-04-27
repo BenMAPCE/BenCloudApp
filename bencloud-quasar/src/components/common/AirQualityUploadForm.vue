30b53a10dddb2171378ba67004ec5badad5a1e4b
Jim Anderton <janderton@indecon.com>
2023-04-27 13:20:17 -0700

    remove debugger reference

--- bencloud-quasar/src/components/common/AirQualityUploadForm.vue
+++ bencloud-quasar/src/components/common/AirQualityUploadForm.vue
@@ -438,7 +438,6 @@ export default {
     },
 
     hybridSelected(value){
-      debugger; //todo:
       if(value.toLowerCase()=="hybrid model")
       {
         this.descriptionHint = "Please enter the description for this hybrid model.";
