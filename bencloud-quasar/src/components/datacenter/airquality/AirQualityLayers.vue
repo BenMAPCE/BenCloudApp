850e81df68f85bab8e3d8e853fe8b6290c07cad2
Adrian Driver <adriver@indecon.com>
2026-05-06 09:47:40 -0700

    BWD-262 - add secondary warning when archiving shared data

--- bencloud-quasar/src/components/datacenter/airquality/AirQualityLayers.vue
+++ bencloud-quasar/src/components/datacenter/airquality/AirQualityLayers.vue
@@ -149,37 +149,38 @@ export default defineComponent({
     archiveRow(props) {
       // Prompt user to confirm AQ layer archiving
       if(confirm("This AQ layer is shared with all users. Are you sure you wish to archive " + props.row.name + "?")){
-        // Archive the AQ layer, reload the list if successful, alert the user if unsuccessful       
-        axios
-          .post(process.env.API_SERVER + "/api/air-quality-data/" + props.row.id, 
-            {validateStatus: function (status) {
-              return status < 500;
-            }}
-          )
-          .then((response) => {
-            if(response.status === 200) {
-              trackCurrentPage = this.pagination.page;
-              console.log("Successfully archived AQ layer: " + props.row.name);
+        if(confirm("This AQ layer is shared with all users. Are you sure?")){
+          // Archive the AQ layer, reload the list if successful, alert the user if unsuccessful       
+          axios
+            .post(process.env.API_SERVER + "/api/air-quality-data/" + props.row.id, 
+              {validateStatus: function (status) {
+                return status < 500;
+              }}
+            )
+            .then((response) => {
+              if(response.status === 200) {
+                trackCurrentPage = this.pagination.page;
+                console.log("Successfully archived AQ layer: " + props.row.name);
 
-              // Reload list
-              var oldValue =  this.$store.state.airquality.airQualityForceReloadValue
-              console.log("oldValue: " + oldValue);
-              var newValue = oldValue - 1;
-              console.log("newValue: " + newValue);
-              this.$store.commit("airquality/updateAirQualityForceReloadValue", newValue)
-            } else if(response.status === 403){
-              console.log("Forbidden action on AQ layer: " + props.row.name);
-              this.$q.notify({
-                group: false, // required to be updateable
-                type: 'negative',
-                timeout: 6000, 
-                color: "red",
-                spinner: false, // we reset the spinner setting so the icon can be displayed
-                position: "top",
-                message: response.data.message,
-              });
-              this.$emit('ok')
-            } else {
+                // Reload list
+                var oldValue =  this.$store.state.airquality.airQualityForceReloadValue
+                console.log("oldValue: " + oldValue);
+                var newValue = oldValue - 1;
+                console.log("newValue: " + newValue);
+                this.$store.commit("airquality/updateAirQualityForceReloadValue", newValue)
+              } else if(response.status === 403){
+                console.log("Forbidden action on AQ layer: " + props.row.name);
+                this.$q.notify({
+                  group: false, // required to be updateable
+                  type: 'negative',
+                  timeout: 6000, 
+                  color: "red",
+                  spinner: false, // we reset the spinner setting so the icon can be displayed
+                  position: "top",
+                  message: response.data.message,
+                });
+                this.$emit('ok')
+              } else {
               this.$q.notify({
                 group: false, // required to be updateable
                 type: 'negative',
@@ -192,6 +193,7 @@ export default defineComponent({
               this.$emit('ok')
             }
           })
+        }
       }
     },
 
