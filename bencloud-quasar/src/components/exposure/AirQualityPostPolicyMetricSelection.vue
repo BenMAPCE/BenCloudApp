4d7fe0ac57a893c84441a81ef8ecd0d248a28ce1
Adrian Driver <adriver@indecon.com>
2026-06-16 11:41:25 -0700

    BWD-281 - add grid definition to "Input file characteristics" table on AQ selection screen

--- bencloud-quasar/src/components/exposure/AirQualityPostPolicyMetricSelection.vue
+++ bencloud-quasar/src/components/exposure/AirQualityPostPolicyMetricSelection.vue
@@ -268,7 +268,7 @@ export default defineComponent({
 
               row = {};
 
-              row.input_file_characteristic = "filename";
+              row.input_file_characteristic = "File name";
               row.value = airQualityLayers[i].filename;
               rows.value.push(row);
 
@@ -290,6 +290,11 @@ export default defineComponent({
                 Math.round(metric_statistics[m].pct_2_5 * 100) / 100 +
                 " - " +
                 Math.round(metric_statistics[m].pct_97_5 * 100) / 100;
+              rows.value.push(row);
+
+              row = {};
+              row.input_file_characteristic = "Grid definition";
+              row.value = airQualityLayers[i].grid_definition_name;
               rows.value.push(row);
             }
           }
